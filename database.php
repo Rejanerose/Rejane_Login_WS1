3787e260ab2c8fed3749e68fadfad0b4a4d77915
Rejanerose <rteves2024@hccci.edu.ph>
2026-09-16 23:09:04 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,14 +1,13 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "portfolio";
+$host = "sql201.infinityfree.com";
+$username = "if0_42933051";
+$password = "Rejanerose27";
+$database = "if0_42933051_login";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
-
 ?>
